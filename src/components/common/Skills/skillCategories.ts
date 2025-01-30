3ae147542c9b2bd54b7db2770a23f4eb67f8ba95
Sami Kuikka <sami.kuikka@aalto.fi>
2025-01-30 18:30:21 +0200

    add databses

--- src/components/common/Skills/skillCategories.ts
+++ src/components/common/Skills/skillCategories.ts
@@ -8,6 +8,10 @@ import dockerLogo from "../../../assets/logos/docker.svg";
 import astroLogo from "../../../assets/logos/astro.svg";
 import pythonLogo from "../../../assets/logos/python.svg";
 import kubernetesLogo from "../../../assets/logos/kubernetes.svg";
+import mongodbLogo from "../../../assets/logos/mongodb.svg";
+import mysqlLogo from "../../../assets/logos/mysql.svg";
+import postgresqlLogo from "../../../assets/logos/postgresql.svg";
+import redisLogo from "../../../assets/logos/redis.svg";
 
 export const allSkillCategories: SkillCategory[] = [
   {
@@ -37,6 +41,16 @@ export const allSkillCategories: SkillCategory[] = [
       { name: "Docker", level: 3, year: 2021, logo: dockerLogo.src },
       { name: "Kubernetes", level: 3, year: 2021, logo: kubernetesLogo.src },
       { name: "CI/CD", level: 3, year: 2023 },
+    ],
+  },
+  {
+    name: "Database",
+    color: "#ff9f43",
+    skills: [
+      { name: "MongoDB", level: 4, year: 2021, logo: mongodbLogo.src },
+      { name: "PostgreSQL", level: 4, year: 2021, logo: postgresqlLogo.src },
+      { name: "MySQL", level: 4, year: 2019, logo: mysqlLogo.src },
+      { name: "Redis", level: 4, year: 2023, logo: redisLogo.src },
     ],
   },
 ];
