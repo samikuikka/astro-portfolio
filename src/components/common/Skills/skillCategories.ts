481f5be07acd317c2c3cfca3754207f03024ef0c
Sami Kuikka <sami.kuikka@aalto.fi>
2025-01-31 20:07:20 +0200

    improve the messages

--- src/components/common/Skills/skillCategories.ts
+++ src/components/common/Skills/skillCategories.ts
@@ -24,7 +24,7 @@ export const allSkillCategories: SkillCategory[] = [
         year: 2019,
         logo: reactLogo.src,
         description:
-          "Used React to build dynamic and responsive user interfaces for various web applications.",
+          "I have been using React since 2019 and since then I have been using it in most of my frontend projects.",
         projects: [
           {
             name: "Portfolio Website",
@@ -44,7 +44,16 @@ export const allSkillCategories: SkillCategory[] = [
         level: 4,
         year: 2019,
         logo: nextjsLogo.src,
-        description: "Next.js is a React framework for production.",
+        description:
+          "Next.js is my go-to framework for server-side rendering and creating applications with great SEO.",
+        projects: [
+          {
+            name: "Language learning blog",
+            description:
+              "A blog site for creating blog posts in target language and getting feedback from native speakers.",
+            url: "https://github.com/samikuikka/www-project",
+          },
+        ],
       },
       {
         name: "Astro",
@@ -66,36 +75,167 @@ export const allSkillCategories: SkillCategory[] = [
           },
         ],
       },
-      { name: "Angular", level: 4, year: 2021, logo: angularLogo.src },
+      {
+        name: "Angular",
+        level: 4,
+        year: 2021,
+        logo: angularLogo.src,
+        description:
+          "Angular is a great framework for building large scale applications. It is framework used in my current job, and I'm most proficient with it. Currently using it to build a cloud based storage solution for construction industry.",
+      },
     ],
   },
   {
     name: "Backend",
     color: "#4ecdc4",
     skills: [
-      { name: "Node.js", level: 5, year: 2019, logo: nodejsLogo.src },
-      { name: "Python", level: 4, year: 2023, logo: pythonLogo.src },
-      { name: "Java", level: 4, year: 2023 },
-      { name: ".NET", level: 4, year: 2023, logo: dotnetLogo.src },
+      {
+        name: "Node.js",
+        level: 5,
+        year: 2019,
+        logo: nodejsLogo.src,
+        description:
+          "For JavaScript based backends, I mostly use Node.js. I'm also most proficient in it from backend tehcnologies.",
+      },
+      {
+        name: "Python",
+        level: 4,
+        year: 2023,
+        logo: pythonLogo.src,
+        description:
+          "Started using Python for serious projects in my work by creating microservice for efficient thumbnail generation using python.",
+      },
+      {
+        name: "Java",
+        level: 4,
+        year: 2023,
+        description:
+          "Java is the language used in my current job. We create reactive microservices with Spring Boot.",
+      },
+      {
+        name: ".NET",
+        level: 4,
+        year: 2023,
+        logo: dotnetLogo.src,
+        description:
+          "I have also some knowledge on .NET, due to it being used in several microservices in the work.",
+      },
     ],
   },
   {
     name: "DevOps",
     color: "#feca57",
     skills: [
-      { name: "Docker", level: 3, year: 2021, logo: dockerLogo.src },
-      { name: "Kubernetes", level: 3, year: 2021, logo: kubernetesLogo.src },
-      { name: "CI/CD", level: 3, year: 2023 },
+      {
+        name: "Docker",
+        level: 3,
+        year: 2021,
+        logo: dockerLogo.src,
+        description:
+          "I have been using Docker in both own projects and in work to run backend services.",
+        projects: [
+          {
+            name: "Chat application",
+            description:
+              "A web based chat application with Kubernetes load-balancing.",
+            url: "https://github.com/samikuikka/Chat-application",
+          },
+          {
+            name: "Code grader",
+            description: "A web based code grader for programming courses.",
+            url: "https://github.com/samikuikka/code-grader",
+          },
+        ],
+      },
+      {
+        name: "Kubernetes",
+        level: 3,
+        year: 2021,
+        logo: kubernetesLogo.src,
+        description:
+          "I wrote my Master's thesis on Kubernetes networking, especially about API gateways and service meshes in Kubernetes.",
+        projects: [
+          {
+            name: "Chat application",
+            description:
+              "A web based chat application with Kubernetes load-balancing.",
+            url: "https://github.com/samikuikka/Chat-application",
+          },
+        ],
+      },
+      {
+        name: "CI/CD",
+        level: 3,
+        year: 2023,
+        description:
+          "I have used CI/CD pipelines in my work to automate testing and deployment of microservices.",
+      },
     ],
   },
   {
     name: "Database",
     color: "#ff9f43",
     skills: [
-      { name: "MongoDB", level: 4, year: 2021, logo: mongodbLogo.src },
-      { name: "PostgreSQL", level: 4, year: 2021, logo: postgresqlLogo.src },
-      { name: "MySQL", level: 4, year: 2019, logo: mysqlLogo.src },
-      { name: "Redis", level: 4, year: 2023, logo: redisLogo.src },
+      {
+        name: "MongoDB",
+        level: 4,
+        year: 2021,
+        logo: mongodbLogo.src,
+        description:
+          "I have used MongoDB in my work to store data for microservices.",
+        projects: [
+          {
+            name: "Workout logger",
+            description:
+              "Backend implementation for workout logging endpoints.",
+            url: "https://github.com/samikuikka/workout-logger-backend",
+          },
+        ],
+      },
+      {
+        name: "PostgreSQL",
+        level: 4,
+        year: 2021,
+        logo: postgresqlLogo.src,
+        description:
+          "I have both professional experience and own interest on SQL based databases.",
+        projects: [
+          {
+            name: "Chat application",
+            description:
+              "A web based chat application with Kubernetes load-balancing.",
+            url: "https://github.com/samikuikka/Chat-application",
+          },
+          {
+            name: "Code grader",
+            description: "A web based code grader for programming courses.",
+            url: "https://github.com/samikuikka/code-grader",
+          },
+        ],
+      },
+      {
+        name: "MySQL",
+        level: 4,
+        year: 2019,
+        logo: mysqlLogo.src,
+        description:
+          "I have both professional experience and own interest on SQL based databases.",
+      },
+      {
+        name: "Redis",
+        level: 4,
+        year: 2023,
+        logo: redisLogo.src,
+        description:
+          "My redis usage so far is for caching information for performance improvements in microservices.",
+        projects: [
+          {
+            name: "Code grader",
+            description: "A web based code grader for programming courses.",
+            url: "https://github.com/samikuikka/code-grader",
+          },
+        ],
+      },
     ],
   },
 ];
